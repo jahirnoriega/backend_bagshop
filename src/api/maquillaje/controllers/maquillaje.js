00b79ede1b1a0c23d9195e83750746d4f76a8c18
Jahir Noriega <64813299+jahirnoriega@users.noreply.github.com>
2024-02-12 13:33:09 -0600

    Deployment

---
+++ src/api/maquillaje/controllers/maquillaje.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * maquillaje controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::maquillaje.maquillaje');
---
+++ src/api/maquillaje/routes/maquillaje.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * maquillaje router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::maquillaje.maquillaje');
